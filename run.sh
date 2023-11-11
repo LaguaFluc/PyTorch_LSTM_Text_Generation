0494ecbfb9a24935d63594d155177ef3d06e5588
zhushanshan <zhushanshan@wps.cn>
2023-11-11 15:18:12 +0800

    add run.sh for GPUShare

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p "Username:" username
+read -s -p "Password:" password
+echo -e "\n"
+
+oss login -u $username -p $password
+
+echo y | conda create -p /hy-tmp/nlp_action python=3.10
+
+#!/bin/bash
+pip install torch
+pip install torchtext
+pip install pyyaml
+pip install portalocker
+
+
