ef5c7eb85b900967a7d2386ecfca4423a8ee3be0
zhushanshan <zhushanshan@wps.cn>
2023-11-12 10:57:08 +0800

    add conda activate to run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ oss login -u $username -p $password
 
 echo y | conda create -p /hy-tmp/nlp_action python=3.10
 
+conda activate /hy-tmp/nlp_action
 #!/bin/bash
 pip install torch
 pip install torchtext
